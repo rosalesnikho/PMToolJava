db1d4908bc9d31ed8f14cf7efb6f80c0e33953de
nikvicros <nikho.rosales@gmail.com>
2019-08-10 16:55:18 -0500

    added validation

--- src/main/java/io/pmtool/pmtool/services/ProjectService.java
+++ src/main/java/io/pmtool/pmtool/services/ProjectService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
-
     public Project saveOrUpdateProject (Project project) {
         return projectRepository.save(project);
     }
