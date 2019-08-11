0fa388cd09c9987d876299467bf6c881872280e5
nikvicros <nikho.rosales@gmail.com>
2019-08-10 21:27:27 -0500

    added update and delete logic

--- src/main/java/io/pmtool/pmtool/services/ProjectService.java
+++ src/main/java/io/pmtool/pmtool/services/ProjectService.java
@@ -27,13 +27,22 @@ public class ProjectService {
     public Project findProjectByIdentifier(String projectId) {
         Project project  = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if(project == null) {
-            throw new ProjectIdException("Project ID " + projectId.toUpperCase() + " does not exist!");
+            throw new ProjectIdException("Project " + projectId.toUpperCase() + " does not exist!");
         }
         return project;
     }
 
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier (String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null) {
+            throw new ProjectIdException("Project " + projectId.toUpperCase() + " does not exist!");
+        }
+        projectRepository.delete(project);
+
     }
 }
 
