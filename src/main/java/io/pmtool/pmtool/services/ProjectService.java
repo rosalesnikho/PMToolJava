1f7eeb3a12bbd8def3cc36b9f0cc6afaa15011b5
nikvicros <nikho.rosales@gmail.com>
2019-08-10 19:22:30 -0500

    added custom try catch and logic

--- src/main/java/io/pmtool/pmtool/services/ProjectService.java
+++ src/main/java/io/pmtool/pmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.pmtool.pmtool.services;
 
 import io.pmtool.pmtool.domain.Project;
+import io.pmtool.pmtool.exceptions.ProjectIdException;
 import io.pmtool.pmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,14 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
+
     public Project saveOrUpdateProject (Project project) {
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exist!");
+        }
     }
 }
 
